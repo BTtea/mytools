05e792886aa889aeac8a98c65721eed67e7d46c8
BTtea <32271123+BTtea@users.noreply.github.com>
2025-07-14 10:19:15 +0800

    Update setup_tool.sh

--- setup_tool.sh
+++ setup_tool.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 while IFS= read -r line; do
+    [[ -z ${line//[[:space:]]/} ]] && continue
+    [[ ${line:0:1} == "#" ]] && continue
     git_tool=${line%% *}
     annotation=${line#* }
     git clone "$git_tool"
