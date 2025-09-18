d4e63e471f32c7c7810d796699cd46363ec8ef76
BTtea <32271123+BTtea@users.noreply.github.com>
2025-09-18 10:07:15 +0800

    Update setup_tool.sh

--- setup_tool.sh
+++ setup_tool.sh
@@ -6,6 +6,9 @@ while IFS= read -r line; do
     annotation=${line#* }
     git clone "$git_tool"
 done < tools.txt
+
+# sudo apt install -y build-essential zlib1g zlib1g-dev libpq-dev libpcap-dev libsqlite3-dev ruby ruby-dev
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb -o msfinstall
 chmod +x msfinstall
+
 # python3 -m pip install -i https://pypi.org/simple/ GitHacker
