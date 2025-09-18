0534cd40042c3d32e5405b73edd9d3935335cc97
BTtea <32271123+BTtea@users.noreply.github.com>
2025-09-18 09:59:34 +0800

    Update setup_tool.sh

--- setup_tool.sh
+++ setup_tool.sh
@@ -6,4 +6,6 @@ while IFS= read -r line; do
     annotation=${line#* }
     git clone "$git_tool"
 done < tools.txt
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb -o msfinstall
+chmod +x msfinstall
 # python3 -m pip install -i https://pypi.org/simple/ GitHacker
